34f8052e8fe95a10684aed2ddb4fcc44b930628d
Awatansa Vishwakarma <ravi.awatansa@gmail.com>
2021-07-29 14:34:45 +0530

    Updates

--- src/lib/bg-animate/index.tsx
+++ src/lib/bg-animate/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import useStyle from './styles.jss'
+import React, { PropsWithChildren } from "react";
+import useStyle from "./styles.jss";
 
-const RawBackgroundAnimation = (props : any)=>{
-    const classes = useStyle()
-    const {children} = props
-    return (
-        <div className={classes.bg}>
-            {children}
-        </div>
-    )
-}
+const RawBackgroundAnimation = ({ children }: PropsWithChildren<any>) => {
+  const classes = useStyle();
+  return (
+    <div className={classes.bg}>
+      {children}
+    </div>
+  );
+};
 
-export { RawBackgroundAnimation }
+export { RawBackgroundAnimation };
 
